355ae23f28dc35deb2b57c71a69ee43378331370
Karel De Vogeleer <arxcruncher@ArxCruncher-MeanMachine.local>
2017-11-15 09:34:15 +0100

    Added weight and balance source code

---
+++ js/aircraft.js
@@ -0,0 +1,23 @@
+var aircraft_data = {
+			"XX-XXX": {
+				type : "Piper Archer III",
+				aircraft_weight : {value : 710.10, unit : "kg"},
+				aircraft_arm : {value : 2.208, unit : "m"},
+				fuel_arm : {value : 2.410, unit : "m"},
+				fuel_max : {value : 48, unit : "USgal"},
+				pilot_arm : {value : 2.040, unit : "m"},
+				passenger_arm : {value : 3.0, unit : "kg"},
+				luggage_arm : {value : 3.630, unit : "m"},
+				luggage_max : {value : 80.0, unit : "kg"},
+				ramp_weight_max : {value : 1125.35, unit : "kg"},
+				takeoff_weight_max : {value : 1225.67, unit : "kg"},
+				default_taxi_fuel : {value : 5, unit : "l"},
+				lb_plot : {x : [2.08,2.08,2.24,2.36,2.36,2.08],
+								y : [500,930,1156.6,1156.6,500,500],
+								check : ["right","right","below","left","above"],
+								ylab : "weight (kg)",
+								xlab : "arm (m)",
+								xaxis : "arm",
+								unit : "metric"}
+			}
+		};
